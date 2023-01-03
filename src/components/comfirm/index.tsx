4d736e827415c5f5de5971dfd1cc5f028502acbf
张成圆 <zhangchengyuan@biosan.cn>
2023-01-03 16:27:28 +0800

    feat: 完善了弹出窗组件

--- src/components/comfirm/index.tsx
+++ src/components/comfirm/index.tsx
@@ -1,22 +1,27 @@
-import React, { MouseEventHandler } from "react";
+import React from "react";
+import ReactDOM from "react-dom";
 import { Props } from "./interface";
 import { Button } from "@/components";
 import styles from "./index.less";
+
+const root = document.getElementById("root") as Element; //插入位置
 
 export function Comfirm(props: Props) {
   const { visible, okText, title, onOk } = props;
   return (
     <>
-      {visible && (
-        <div className={styles.mask}>
-          <div className={styles.comfirm}>
-            <div className={styles["comfirm-body"]}>{title}</div>
-            <div className={styles["comfirm-btns"]}>
-              <Button onClick={onOk}>{okText}</Button>
+      {visible &&
+        ReactDOM.createPortal(
+          <div className={styles.mask}>
+            <div className={styles.comfirm}>
+              <div className={styles["comfirm-body"]}>{title}</div>
+              <div className={styles["comfirm-btns"]}>
+                <Button onClick={onOk}>{okText}</Button>
+              </div>
             </div>
-          </div>
-        </div>
-      )}
+          </div>,
+          root
+        )}
     </>
   );
 }
